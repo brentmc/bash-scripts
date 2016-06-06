cdee192af2efb9f7d6477496e999232e6e415e63
brentmc <brentmcivor.mail+workgithub@gmail.com>
2016-06-06 14:31:52 +1000

    CobraGit - Now opens up each repo in a new tab. Also have the option to smart-pull

--- open-all-cobra-gits.sh
+++ open-all-cobra-gits.sh
@@ -18,9 +18,80 @@
 ## To run multiple commands you need to do eval then wrap all commands in SINGLE quotes
 ## Put a semicolon between each command
 ##############################################################################################################
-ttab -t "Cobra App"     eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-app; gitx'
-ttab -t "Cobra Apps"    eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-apps; gitx'
-ttab -t "Cobra Engine"  eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-engine; gitx'
-ttab -t "Cobra Games"   eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-games; gitx'
-ttab -t "Cobra Tools"   eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-tools; gitx'
-ttab -t "Cobra UI"      eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-ui; gitx'
+#ttab -t "Cobra App"     eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-app; gitx'
+#ttab -t "Cobra Apps"    eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-apps; gitx'
+
+
+cobraFolder='/Users/brentmcivor/Intrepica/cobra/'
+app='cobra-app'
+apps='cobra-apps'
+engine='cobra-engine'
+games='cobra-games'
+tools='cobra-tools'
+ui='cobra-ui'
+reposAr=($app $apps $engine $games $tools $ui)
+
+######################################################
+# Opens all repos in a new tab then opens gitx
+# If you pass through true it will also run
+# $git-smart-pull for each repo to update everything
+######################################################
+function openAll {
+  needsToPullDown=$1
+
+	for i in "${reposAr[@]}"
+	do
+		repoFolder=$i
+		tabTitle=$i
+
+		command='cd '$cobraFolder$repoFolder'; gitx;'
+
+		if $needsToPullDown
+		then
+			command=$command' git-smart-pull'
+		fi
+
+		#echo $command
+		ttab -t $tabTitle eval $command
+	done
+}
+
+######################################################
+# To protect from accidentally pulling down over any
+# uncommitted changes we confirm the query
+######################################################
+function confirmUpdate {
+	echo "This will pull down over existing code. Are you sure you want to do this?"
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        "Yes" )
+	            openAll true
+	            break;;
+	        "No" )
+	            exit;;
+	    esac
+	done
+}
+
+######################################################
+# First question that kicks everything off
+# You can choose to open up gitx for each repo
+# or open up gitx for each AND pull down all changes
+######################################################
+function firstQuestion {
+	echo "Do you want to pull down the latest changes?"
+	select yn in "Yes" "No" "Exit"; do
+	    case $yn in
+	        "Yes" )
+	            confirmUpdate
+	            break;;
+	        "No" )
+	            openAll false
+	            break;;
+	        "Exit" )
+	            exit;;
+	    esac
+	done
+}
+
+firstQuestion
