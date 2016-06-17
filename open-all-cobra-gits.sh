1b6e499cd07bdfeb1c9b2e67f485d22b68ec204d
brentmc <brentmcivor.mail+workgithub@gmail.com>
2016-06-17 11:33:45 +1000

    Now opens, pulls and pushes from one file
    You can now pass through arguments to decide if we should open, pull or push all repos

--- open-all-cobra-gits.sh
+++ open-all-cobra-gits.sh
@@ -26,30 +26,45 @@ cobraFolder='/Users/brentmcivor/Intrepica/cobra/'
 app='cobra-app'
 apps='cobra-apps'
 engine='cobra-engine'
+exercise='cobra-exercise'
 games='cobra-games'
 lpAPI='cobra-lp-api'
 tools='cobra-tools'
 ui='cobra-ui'
-reposAr=($app $apps $engine $games $lpAPI $tools $ui)
+reposAr=($app $apps $engine $exercise $games $lpAPI $tools $ui)
 
-######################################################
+function openGitTower {
+	command='gittower '$cobraFolder$games
+	ttab -t 'GitTower' eval $command
+}
+#########################################################################
 # Opens all repos in a new tab then opens gitx
 # If you pass through true it will also run
 # $git-smart-pull for each repo to update everything
-######################################################
+#########################################################################
 function openAll {
   needsToPullDown=$1
+  needsToPush=$2
+
+	openGitTower
 
 	for i in "${reposAr[@]}"
 	do
 		repoFolder=$i
-		tabTitle=$i
+		tabTitle=${i#'cobra-'} #strips "cobra-" from the start of the String
 
-		command='cd '$cobraFolder$repoFolder'; gitx;'
+		#Going to try out GitTower now instead of opening up a heap of gitx windows
+		#command='cd '$cobraFolder$repoFolder'; gitx;'
+		command='cd '$cobraFolder$repoFolder';'
 
 		if $needsToPullDown
 		then
-			command=$command' git-smart-pull'
+			command=$command' git-smart-pull;'
+		fi
+
+		if $needsToPush
+		then
+			command=$command' git push origin master;'
 		fi
 
 		#echo $command
@@ -57,16 +72,17 @@ function openAll {
 	done
 }
 
-######################################################
-# To protect from accidentally pulling down over any
-# uncommitted changes we confirm the query
-######################################################
-function confirmUpdate {
-	echo "This will pull down over existing code. Are you sure you want to do this?"
+#########################################################################
+# To protect from accidentally pulling down over any uncommitted changes
+# we confirm the query
+#########################################################################
+function confirmPull {
+	echo "This will pull down over existing code."
+	echo "Are you sure you want to do this?"
 	select yn in "Yes" "No"; do
 	    case $yn in
 	        "Yes" )
-	            openAll true
+	            openAll true false
 	            break;;
 	        "No" )
 	            exit;;
@@ -74,25 +90,42 @@ function confirmUpdate {
 	done
 }
 
-######################################################
-# First question that kicks everything off
-# You can choose to open up gitx for each repo
-# or open up gitx for each AND pull down all changes
-######################################################
-function firstQuestion {
-	echo "Do you want to pull down the latest changes?"
-	select yn in "Yes" "No" "Exit"; do
+#########################################################################
+# To protect from accidentally pulling down over any uncommitted changes
+# or pushing to origin master before we are ready,
+# we confirm the query
+#########################################################################
+function confirmPush {
+	echo "This will pull down over existing code. Then push to remote master."
+	echo "Are you sure you want to do this?"
+	select yn in "Yes" "No"; do
 	    case $yn in
 	        "Yes" )
-	            confirmUpdate
+	            openAll true true
 	            break;;
 	        "No" )
-	            openAll false
-	            break;;
-	        "Exit" )
 	            exit;;
 	    esac
 	done
 }
 
-firstQuestion
+
+#########################################################################
+# This is the first stuff that happens
+# Call this file then pass through an argument
+# ./open-all-cobra-gits.sh -open (opens GitTower and each repo in an iTerm tab)
+# ./open-all-cobra-gits.sh -pull (opens then pulls down)
+# ./open-all-cobra-gits.sh -push (opens, pulls down then pushes up)
+#########################################################################
+key="$1"
+case $key in
+	-open)
+	openAll false false
+	;;
+	-pull)
+	confirmPull
+	;;
+	-push)
+	confirmPush
+	;;
+esac
