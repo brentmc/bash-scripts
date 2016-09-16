99b716966a0ee1f35662c8f6afea9409ea9d745a
brentmc <brentmcivor.mail+workgithub@gmail.com>
2016-09-16 10:38:14 +1000

    Now create and delete tags!
    I wanted to add creation and deletion of tags to all repos both locally and remotely. My existing method was getting a little out of hand so I completely refactored everything so I wasn’t passing a billion booleans through to the main function

--- open-all-cobra-gits.sh
+++ open-all-cobra-gits.sh
@@ -10,6 +10,8 @@
 #           -- alias pullobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -pull"
 #           -- alias pushcobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -push"
 #           -- alias eslintcobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -eslint"
+#           -- alias tagcobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -tag"
+#           -- alias deletetagcobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -deleteTag"
 #           -- alias checkoutcobra="cd ~/Intrepica/brents_scripts/ && ./open-all-cobra-gits.sh -checkout"
 #		-- run $ opencobra
 
@@ -26,80 +28,53 @@
 #ttab -t "Cobra Apps"    eval 'cd /Users/brentmcivor/Intrepica/cobra/cobra-apps; gitx'
 
 
-cobraFolder='/Users/brentmcivor/Intrepica/cobra/'
-app='cobra-app'
-apps='cobra-apps'
-engine='cobra-engine'
-exercise='cobra-exercise'
-games='cobra-games'
-lpAPI='cobra-lp-api'
-tools='cobra-tools'
-ui='cobra-ui'
+cobraFolder="/Users/brentmcivor/Intrepica/cobra/"
+app="cobra-app"
+apps="cobra-apps"
+engine="cobra-engine"
+exercise="cobra-exercise"
+games="cobra-games"
+lpAPI="cobra-lp-api"
+tools="cobra-tools"
+ui="cobra-ui"
 reposAr=($app $apps $engine $exercise $games $lpAPI $tools $ui)
 
-function openGitTower {
-	command='gittower '$cobraFolder$games
-	ttab -t 'GitTower' eval $command
-}
 #########################################################################
-# Opens all repos in a new tab then opens gitx
-# If you pass through true it will also run
-# $git-smart-pull for each repo to update everything
+# Opens all repos in a new tab then runs the given command
 #########################################################################
-function openAll {
-  needsToPullDown=$1
-  needsToPush=$2
-  needsToLint=$3
-  needsToCheckout=$4
-  branchToCheckout=$5
-
-	## openGitTower No longer want to open GitTower automatically
+function run {
+	command="$1"
 
 	for i in "${reposAr[@]}"
 	do
 		repoFolder=$i
-		tabTitle=${i#'cobra-'} #strips "cobra-" from the start of the String
+		tabTitle=${i#'cobra-'} #strips 'cobra-' from the start of the String
 
-		#Going to try out GitTower now instead of opening up a heap of gitx windows
-		#command='cd '$cobraFolder$repoFolder'; gitx;'
-		command='cd '$cobraFolder$repoFolder';'
+		completeCommand='cd '$cobraFolder$repoFolder'; '"$command"
 
-		if $needsToPullDown
-		then
-			command=$command' git-smart-pull;'
-		fi
-
-		if $needsToPush
-		then
-			command=$command' git push origin master;'
-		fi
-
-		if $needsToLint
-		then
-			command=$command' eslint ./*;'
-		fi
-
-		if $needsToCheckout
-		then
-			command=$command' git checkout '$branchToCheckout';'
-		fi
-
-		#echo $command
-		ttab -t $tabTitle eval $command
+		#echo $completeCommand
+		ttab -t $tabTitle eval $completeCommand
 	done
 }
 
+
 #########################################################################
+# Pulls down master for all repos
+#
 # To protect from accidentally pulling down over any uncommitted changes
 # we confirm the query
 #########################################################################
+function runPull {
+	run 'git-smart-pull;'
+}
+
 function confirmPull {
 	echo "This will pull down over existing code."
 	echo "Are you sure you want to do this?"
 	select yn in "Yes" "No"; do
 	    case $yn in
 	        "Yes" )
-	            openAll true false false
+	            runPull
 	            break;;
 	        "No" )
 	            exit;;
@@ -112,13 +87,17 @@ function confirmPull {
 # or pushing to origin master before we are ready,
 # we confirm the query
 #########################################################################
+function runPush {
+	run 'git-smart-pull; git push origin master;'
+}
+
 function confirmPush {
 	echo "This will pull down over existing code. Then push to remote master."
 	echo "Are you sure you want to do this?"
 	select yn in "Yes" "No"; do
 	    case $yn in
 	        "Yes" )
-	            openAll true true false
+	            runPush
 	            break;;
 	        "No" )
 	            exit;;
@@ -127,17 +106,85 @@ function confirmPush {
 }
 
 #########################################################################
+# Lint all repos at once
+#########################################################################
+function runLint {
+	run 'eslint ./*;'
+}
+
+#########################################################################
+# Tag the same version and commit message on all repos at once
+# You need to wrap the message in double quotes otherwise it loses everything
+# after the first space
+#########################################################################
+function runTag {
+	tag="$1"
+	commitMsg="$2"
+	echo $tag
+	echo $commitMsg
+	run 'git tag -a '"$tag"' -m '\""$commitMsg"\"'; git push --tags;'
+}
+
+function createTag {
+	echo "This will create and push a tag on all repos at once."
+	echo "Enter version number. (e.g. 1.2.3-ios)"
+	read versionNum
+	tag="sw/v"$versionNum #so we don't have to add the sightwords and ver. prefix
+
+	echo "Enter commit message."
+	read commitMsg
+
+	echo "Tag:" $tag "Message:" $commitMsg
+	echo "Is this correct?"
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        "Yes" )
+	            runTag $tag "$commitMsg"
+	            break;;
+	        "No" )
+	            exit;;
+	    esac
+	done
+}
+
+#########################################################################
+# Deletes a tag locally and remotely from all repos
+#########################################################################
+function runDeleteTag {
+	tag="$1"
+	run 'git tag -d '$tag'; git push origin :refs/tags/'$tag
+}
+
+function deleteTag {
+	echo "This will delete a tag both locally and remotely from all repos;"
+	echo "Which tag do you want to delete? (e.g. 1.2.3-ios)"
+	read versionNum
+	tag="sw/v"$versionNum #so we don't have to add the sightwords and ver. prefix
+
+	echo "Delete "$tag" locally and remotely from all repos?"
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        "Yes" )
+	            runDeleteTag $tag
+	            break;;
+	        "No" )
+	            exit;;
+	    esac
+	done
+}
+#########################################################################
 # Use these checkout functions to quickly get all repos to a particular
 # release tag or back to master
 #########################################################################
-function checkoutTag {
-	echo "Which tag do you want to checkout?"
-	read tag
-	openAll false false false true 'tags/'$tag
+function runCheckout {
+	branchToCheckout="$1"
+	run 'git checkout '$branchToCheckout';'
 }
 
-function checkoutMaster {
-	openAll false false false true "master"
+function checkoutTag {
+	echo "Which tag do you want to checkout? (e.g. 1.2.3-ios)"
+	read tag
+	runCheckout 'tags/sw/v'$tag
 }
 
 function queryCheckout {
@@ -146,7 +193,7 @@ function queryCheckout {
 	select branch in "Master" "Tagged Commit" "Exit"; do
 		case $branch in
 			"Master" )
-				checkoutMaster
+				runCheckout "master"
 				break;;
 			"Tagged Commit" )
 				checkoutTag
@@ -160,25 +207,32 @@ function queryCheckout {
 #########################################################################
 # This is the first stuff that happens
 # Call this file then pass through an argument
-# ./open-all-cobra-gits.sh -open (opens GitTower and each repo in an iTerm tab)
+# ./open-all-cobra-gits.sh -open (opens each repo in an iTerm tab)
 # ./open-all-cobra-gits.sh -pull (opens then pulls down)
 # ./open-all-cobra-gits.sh -push (opens, pulls down then pushes up)
 # ./open-all-cobra-gits.sh -eslint (opens, then checks eslint)
+# ./open-all-cobra-gits.sh -tag (opens, then tags all repos with given tag and message)
 # ./open-all-cobra-gits.sh -checkout (opens, then checks out the given tag)
 #########################################################################
 key="$1"
 case $key in
 	-open)
-	openAll false false false
+	run
 	;;
 	-eslint)
-	openAll false false true
+	runLint
 	;;
 	-pull)
 	confirmPull
 	;;
 	-push)
 	confirmPush
+	;;
+	-tag)
+	createTag
+	;;
+	-deleteTag)
+	deleteTag
 	;;
 	-checkout)
 	queryCheckout
